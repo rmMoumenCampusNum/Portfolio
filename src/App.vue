eaa70da231f2d311eadecb85123c6a5975ced771
Moumen <moumen.bourahla@le-campus-numerique.fr>
2024-10-19 19:43:09 +0200

    Ajout du titre

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <script setup>
-import Caroussel from '../src/components/Caroussel.vue'
 
 </script>
 
 <template>
-  <Caroussel/>
+  <div class="setTitle">
+<h1>
+  <span class="left">Portfolio</span>
+  <span class="right">Moumen</span></h1>
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
